0e7b9cdb313ef1728e7c8cc8672f9f10e8be6a30
Vedant Datey <65365823+vedant2608@users.noreply.github.com>
2020-11-26 22:28:57 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,32 +1,8 @@
 import random
 change_dict={
-      'a':'z',
-      'b':'y',
-      'c':'x',
-      'd':'w',
-      'e':'v',
-      'f':'u',
-      'g':'t',
-      'h':'s',
-      'i':'r',
-      'j':'q',
-      'k':'p',
-      'l':'o',
-      'm':'n',
-      'n':'m',
-      'o':'l',
-      'p':'k',
-      'q':'j',
-      'r':'i',
-      's':'h',
-      't':'g',
-      'u':'f',
-      'v':'e',
-      'w':'d',
-      'x':'c',
-      'y':'b',
-      'z':'a',
-      ' ':'1010'
+      'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u', 'g':'t','h':'s','i':'r','j':'q',
+      'k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'g',
+      'u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':'1010'
     }
 ######Encoding the text###############
 def Encode():
@@ -55,4 +31,3 @@ def Decode():
                  lst.append(z)
     print("".join(lst))
 
-Decode()
